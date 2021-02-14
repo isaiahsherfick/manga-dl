ecc805c72bbb099af642cfa079c4932256ca549c
isaiah <isaiahsherfick8@gmail.com>
2021-02-14 16:10:12 -0500

    Weird bug with grabbing chapters in other languagesS

--- manga_dl.py
+++ manga_dl.py
@@ -1,6 +1,41 @@
 import requests #to get images from web
 import shutil #to save it locally
+import webcrawler
 
+print("Initializing webcrawler object to crawl https://www.mangadex.org")
+crawler = webcrawler.MyWebCrawler("https://mangadex.org/title/6272/jojo-s-bizarre-adventure-part-7-steel-ball-run-official-colored",1)
+
+#populates crawler.chapters
+crawler.crawl()
+crawler.sortChapters()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 ## Set up the image URL and filename
 image_url = "https://9nrdzhwmm5q6e.9tx6ed0zftkz4.mangadex.network:44199/TrG7TGfCpjIxwISB5JIEF5IgOUR0qI-FpqnWzBJbGgxTWlv6enY6SsIYmfawn12PtahT6vAludTBZDErfZ4WOa5V1gkTiBEeesHOWapx_moQjzk0xAWNrlzqsuAuKpMh7_2e3B8iZlpogbNZ0XZEcIYsKN-zaX2Kp2q9et4aP_ukFYlvB-blgJryXNBOxuDOCOlJxWn8uk18VjxBt_yWYQr8P9N-vA/data/088583bf54406c94bbb90c15af4a38bc/x1.jpg"
 filename = image_url.split("/")[-1]
@@ -18,3 +53,4 @@ if r.status_code == 200:
     print('Image sucessfully Downloaded: ',filename)
 else:
     print('Image Couldn\'t be retreived')
+    '''
--- webcrawler.py
+++ webcrawler.py
@@ -20,47 +20,63 @@ class AnchorParser(HTMLParser):
     def getLinks(self):
         return self.pageLinks
 
-    def handle_starttag(slef, tag, attrs):
+    def handle_starttag(self, tag, attrs):
         if tag == "a":
             for (attribute, value) in attrs:
                 if attribute == "href":
                     #handles case of relative/absolute pathing inconsistency
                     absoluteUrl = urljoin(self.baseURL, value)
                     if urlparse(absoluteUrl).scheme in ["http","https"]:
-                        self.pageLinks.add(absoluteUrl)
+                        if ("https://mangadex.org/chapter" in absoluteUrl) and not ("comments" in absoluteUrl):
+                            self.pageLinks.add(absoluteUrl)
+
+
+
 
 
 class MyWebCrawler(object):
     def __init__(self, url, maxCrawl=10):
         self.visited = set()
+        self.chapters = set()
         self.starterUrl = url
         self.max = maxCrawl
 
-        def getVisited(self):
-            return self.visited
+    def getVisited(self):
+        return self.visited
 
-        def parse():
-            try:
-                # that gibberish about context is used to ignore cert validation
-                #when making the web request
-                htmlContent = urlopen(url,
-                        context=_create_unverified_context()).read().decode()
-                parser = AnchorParser(url)
-                parser.feed(htmlContent)
-                return parser.getLinks()
-            except (HTTPError, InvalidURL, UnicodeDecodeError):
-                print("FAILED: {}".format(url))
+    def parse(self, url):
+        try:
+            # that gibberish about context is used to ignore cert validation
+            #when making the web request
+            htmlContent = urlopen(url,
+                    context=_create_unverified_context()).read().decode()
+            parser = AnchorParser(url)
+            parser.feed(htmlContent)
+            for link in parser.getLinks():
 
-                #returns an empty set
-                return set()
+            return parser.getLinks()
+        except (HTTPError, InvalidURL, UnicodeDecodeError):
+            print("FAILED: {}".format(url))
 
-        def crawl():
-            urlsToParse = {self.starterUrl}
-            while(len(urlsToParse) > 0 and len(self.visited) < self.max):
-                nextUrl = urlsToParse.pop()
-                if nextUrl not in self.visited:
-                    self.visited.add(nectUrl)
-                    print("Parsing: {}".format(nextUrl))
-                    urlsToParse |= self.parse(nextUrl)
+            #returns an empty set
+            return set()
 
+    def crawl(self):
+        urlsToParse = {self.starterUrl}
+        while(len(urlsToParse) > 0 and len(self.visited) < self.max):
+            nextUrl = urlsToParse.pop()
+            if nextUrl not in self.visited:
+                self.visited.add(nextUrl)
+                if "chapter/" in nextUrl:
+                    self.chapters.add(nextUrl.rpartition('/')[2])
+                    print("adding {} to chapters!".format(nextUrl.rpartition('/')[2]))
+                print("Parsing: {}".format(nextUrl))
+                urlsToParse |= self.parse(nextUrl)
 
+    def sortChapters(self):
+        self.chapters = sorted(self.chapters)
+        for i in range(len(self.chapters)):
+            self.chapters[i] = int(self.chapters[i])
+        self.chapters = sorted(self.chapters)
+        print("chapters after calling sort: {}".format(self.chapters))
+        print("len(chapters)={}".format(len(self.chapters)))
